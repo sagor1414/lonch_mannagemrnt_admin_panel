719eef8133aa20e032c67ce2c44cf9e9e8e0223c
sagor <85604284+sagor1414@users.noreply.github.com>
2023-11-06 21:50:49 +0600

    Update about.php

--- about.php
+++ about.php
@@ -23,49 +23,6 @@
             <p class="role">Backend Developer</p>
             <p> Back-end developers are the experts who build and maintain the mechanisms that process data and perform actions on websites. </p>
         </div>
-
-        <div class="team_member">
-           <div class="team_img">
-            <img src="img/bs.png" alt="team_member_image">
-           </div>
-                
-            <h3> Brishti Sarker</h3>
-            <p class="role">Front-End Designer</p>
-            <p> A front-end designer is somebody who ensures a good user experience executing both a great visual design and code.</p>
-        </div>
-
-        
-        <div class="team_member">
-            <div class="team_img">
-                <img src="img/alahi.png" alt="team_member_image">
-               </div>
-              
-            <h3> Abdur Rahman Alahi</h3>
-            <p class="role">schema Designed</p>
-            <p>Database schema design organizes the data into separate entities, determines how to create relationships between organized entities. </p>
-        </div>
-
-        
-        <div class="team_member">
-            <div class="team_img">
-                <img src="img/abir.png" alt="team_member_image">
-               </div>
-                
-            <h3> Sajjatul Islam Abir</h3>
-            <p class="role">Front-End Designer</p>
-            <p>A front-end designer is somebody who ensures a good user experience executing both a great visual design and code.</p>
-        </div>
-
-        
-        <div class="team_member">
-            <div class="team_img">
-                <img src="img/maria.png" alt="team_member_image">
-               </div>
-
-            <h3> Maksuda Akhter Maria</h3>
-            <p class="role">Front-End Designer</p>
-            <p> A front-end designer is somebody who ensures a good user experience executing both a great visual design and code.</p>
-        </div>
     </div>
    </div> 
 </body>
